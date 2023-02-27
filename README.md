ffc31889467163b6198cbfd9fcec4285bfe3bd6c
yaremchuken <yaremchuken@yandex-team.ru>
2023-02-27 15:49:52 +0300

    Switch to Yandex Cloud Storage

--- README.md
+++ README.md
@@ -29,18 +29,22 @@ CREATE DATABASE fitness_terra
 ```
 <a href="https://www.liquibase.com" target="_blank">Liquibase</a> will take care of tables creation
 
-### <span style="color:yellow">Initialize Amazon S3 Storage</span>
+### <span style="color:yellow">Initialize S3 Storage</span>
 
-You will need <a href="http://aws.amazon.com" target="_blank">Amazon AWS</a> account
+For Amazon S3 you will need <a href="http://aws.amazon.com" target="_blank">Amazon AWS</a> account
 and S3 bucket (you can use <a href="https://s3.console.aws.amazon.com/s3/buckets" target="_blank">S3 Management console</a>).
 Don`t forget to create IAM User with S3 permissions in *Security Credentials* of your account.<br>
+Or you can use <a href="http://cloud.yandex.ru/services/storage" target="_blank">Yandex Object Storage</a><br>
 Fill <b>application.yaml</b> with Amazon AWS properties:
 ``` yaml
 cloud:
   aws:
+    # Use endpoint property only for Yandex Storage
+    endpoint: storage.yandexcloud.net  
     bucket: fitness-terra
     region:
-      static: eu-north-1
+      # For Amazon S3 use region according to bucket location
+      static: ru-central1
       auto: false
 ```
 also you need add credentials for Amazon AWS in <b>application.yaml</b>
--- src/main/kotlin/yaremchuken/fitnessterra/config/AWSConfig.kt
+++ src/main/kotlin/yaremchuken/fitnessterra/config/AWSConfig.kt
@@ -2,8 +2,10 @@ package yaremchuken.fitnessterra.config
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider
 import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,13 +14,21 @@ import org.springframework.context.annotation.Configuration
 class AWSConfig(
     @Value("\${cloud.aws.credentials.access-key}") private val accessKey: String,
     @Value("\${cloud.aws.credentials.secret-key}") private val secretKey: String,
+    @Value("\${cloud.aws.endpoint:}") private val endpoint: String,
     @Value("\${cloud.aws.region.static}") private val region: String
 ) {
     @Bean
     fun amazonS3(): AmazonS3 =
-        AmazonS3ClientBuilder
-            .standard()
-            .withRegion(region)
-            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
-            .build()
+        if (StringUtils.isBlank(endpoint))
+            AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+                .withRegion(region)
+                .build()
+        else
+            AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+                .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .build()
 }
